d23827dcce2e74f2400346c1e573df634083cc80
dancinnamon-okta <dan.cinnamon@okta.com>
2018-11-04 21:07:28 -0600

    Made the okta user schema dynamic since it depends on the app's name in Okta.

--- django_scim/constants.py
+++ django_scim/constants.py
@@ -1,3 +1,4 @@
+import os
 ENCODING = 'utf-8'
 SCIM_CONTENT_TYPE = 'application/scim+json'
 BASE_PATH = 'https://localhost'
@@ -7,7 +8,7 @@ class SchemaURI(object):
     LIST_RESPONSE = 'urn:scim:schemas:core:1.0'
     SEARCH_REQUEST = 'urn:ietf:params:scim:api:messages:2.0:SearchRequest'
     USER = 'urn:scim:schemas:core:1.0'
-    OKTA_USER = 'urn:okta:zimtfinancialservicesdancinnamonsdemoorg_legacyappproductcatalogdev_1:1.0:user:custom'
+    OKTA_USER = 'urn:okta:{}:1.0:user:custom'.format(os.environ.get('OKTA_APP_NAME'))
     GROUP = 'urn:scim:schemas:core:1.0'
     RESOURCE_TYPE = 'urn:ietf:params:scim:schemas:core:2.0:ResourceType'
     SERVICE_PROVIDER_CONFIG = 'urn:ietf:params:scim:schemas:core:2.0:ServiceProviderConfig'
