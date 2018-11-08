120dcd7d88cd914703b8ef364ace96a10c866662
dancinnamon-okta <dan.cinnamon@okta.com>
2018-11-07 22:54:30 -0600

    Updated the admin page with groups and a return to home button.

--- swa_app/views.py
+++ swa_app/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.views.decorators.csrf import csrf_exempt
 from django.urls import reverse
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from django.http import HttpResponseForbidden
 
 import json
@@ -21,7 +21,8 @@ def view_admin(request):
     if _is_logged_in(request):
         u = User.objects.get(username=request.user)
         if _is_admin(u):
-            ctx = {'all_users': json.dumps(_get_all_users())}
+            ctx = {'all_users': json.dumps(_get_all_users()),
+                   'all_groups': json.dumps(_get_all_groups())}
             return render(request, 'swa_app/admin.html', ctx)
         else:
             return HttpResponseForbidden()
@@ -77,8 +78,23 @@ def _get_user_profile(usr):
     user_dict['groups'] = dicts
     return user_dict
 
+def _get_group_info(grp):
+    grp_dict = {}
+    grp_dict['name'] = grp.name
+    grp_dict['members'] = []
+    for mem in User.objects.filter(groups__name=grp.name):
+        grp_dict['members'].append(mem.username)
+
+    return grp_dict
+
 def _get_all_users():
     retVal = []
     for usr in User.objects.all():
         retVal.append(_get_user_profile(usr))
+    return retVal
+
+def _get_all_groups():
+    retVal = []
+    for grp in Group.objects.all():
+        retVal.append(_get_group_info(grp))
     return retVal
