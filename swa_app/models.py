d29b9a43c844158e7d1407da243ea4da8ca1daa5
dancinnamon-okta <dan.cinnamon@okta.com>
2018-12-03 23:11:35 -0600

    Made the custom fields not required.

--- swa_app/models.py
+++ swa_app/models.py
@@ -5,11 +5,11 @@ from django.dispatch import receiver
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    department = models.CharField(max_length=256, blank=True)
-    company_name = models.CharField(max_length=30, blank=True)
-    phone_number = models.CharField(max_length=20, blank=True)
-    country = models.CharField(max_length=2, blank=True)
-    opt_in = models.CharField(max_length=3, blank=True)
+    department = models.CharField(max_length=256, blank=True, null=True)
+    company_name = models.CharField(max_length=30, blank=True, null=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    country = models.CharField(max_length=2, blank=True, null=True)
+    opt_in = models.CharField(max_length=3, blank=True, null=True)
 
     @receiver(post_save, sender=User)
     def create_user_profile(sender, instance, created, **kwargs):
